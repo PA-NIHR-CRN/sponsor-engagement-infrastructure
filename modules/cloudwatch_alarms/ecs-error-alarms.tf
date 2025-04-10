14205826986061df2bd1b67b633f3be454ae9e41
akadawg <amarpreetkdawgotra@hotmail.com>
2025-04-10 14:57:40 +0100

    feat: add invitation monitor

--- modules/cloudwatch_alarms/ecs-error-alarms.tf
+++ modules/cloudwatch_alarms/ecs-error-alarms.tf
@@ -108,3 +108,40 @@ resource "aws_cloudwatch_metric_alarm" "notify_error_alarm" {
     System      = var.app
   }
 }
+
+# ECS INVITATION-MONITOR ALARM
+
+resource "aws_cloudwatch_log_metric_filter" "error_filter" {
+  name           = "${var.account}-cloudwatch-${var.env}-${var.app}-invitation-monitor-error-filter"
+  pattern        = "{$.level = 50}"
+  log_group_name = var.invitation_monitor_log_group
+
+  metric_transformation {
+    name      = "ErrorCount"
+    namespace = "SE/${var.env}-se-ecs-invitation-monitor-errors"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "error_alarm" {
+  alarm_name          = "${var.account}-cloudwatch-${var.env}-${var.app}-invitation-monitor-error-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "ErrorCount"
+  namespace           = "SE/${var.env}-se-ecs-invitation-monitor-errors"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "1"
+
+  alarm_description = "Alarm when an error has occured for the Sponsor Engagement invitation-monitor daily task"
+  alarm_actions     = var.env == "prod" ? [var.sns_topic, var.sns_topic_service_desk] : [var.sns_topic]
+  ok_actions        = var.env == "prod" ? [var.sns_topic, var.sns_topic_service_desk] : [var.sns_topic]
+
+  treat_missing_data = "notBreaching"
+
+  tags = {
+    Name        = "${var.account}-cloudwatch-${var.env}-${var.app}-invitation-monitor-error-alarm"
+    Environment = var.env
+    System      = var.app
+  }
+}
